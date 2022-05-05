ed121861857efb75beead8dfd481f4466648808a
dev-tsf4 <dev.tsf4@gmail.com>
2022-05-05 14:13:52 +0200

    fix validate request method post add

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\PostAddMessageRequest;
+use App\Mail\ContactMail;
 use App\Models\Message;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -14,7 +16,14 @@ class ContactController extends Controller
 
     public function postAdd(PostAddMessageRequest $request)
     {
-        Message::create($request->all());
+        Message::create($request->validated());
+
+        Mail::to($request->get('email'))->send(new ContactMail(
+            $request->get('first_name'),
+            $request->get('last_name'),
+            $request->get('email'),
+            $request->get('content')
+        ));
 
         return redirect()->route('contact.index')
             ->with('success','Merci, votre message a bien été envoyé !');
