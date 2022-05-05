69f58f833f01f85783defd8dbc20ee4e49db489c
dev-tsf4 <dev.tsf4@gmail.com>
2022-05-05 09:16:34 +0200

    feat[message]: add model and migration

---
+++ database/migrations/2022_05_05_065858_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 60);
+            $table->string('last_name', 60);
+            $table->string('email', 255);
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
